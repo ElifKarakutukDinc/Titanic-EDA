ceef300591939bc4c7e585d07e5573bee77b5701
ElifKarakutukDinc <elifkaraktk@gmail.com>
2021-03-04 11:16:29 +0100

    adding new function in util_repporting.py and preparing binary relationships in EDA-function file

--- util_reporting.py
+++ util_reporting.py
@@ -121,6 +121,7 @@ def multiple_plot_viz(
     edgecolor="black",
     color_1="orange",
     color_2="blue",
+    bins=20
 ):
     """
     This function gets a Python Pandas dataframe and visualize two countplots.
@@ -137,19 +138,18 @@ def multiple_plot_viz(
     f, ax = plt.subplots(1, 2, figsize=(15, 5))
 
     data[data[column_for_separate] == 0][column_for_calculate].plot.hist(
-        ax=ax[0], bins=20, edgecolor=edgecolor, color=color_1
+        ax=ax[0], bins=bins, edgecolor=edgecolor, color=color_1
     )
     ax[0].set_title(title_1)
     x1 = list(range(0, 85, 5))
     ax[0].set_xticks(x1)
 
     data[data[column_for_separate] == 1][column_for_calculate].plot.hist(
-        ax=ax[1], bins=20, edgecolor=edgecolor, color=color_2
+        ax=ax[1], bins=bins, edgecolor=edgecolor, color=color_2
     )
     ax[1].set_title(title_2)
     x2 = list(range(0, 85, 5))
     ax[1].set_xticks(x2)
-
 
 def crosstab_viz(data, index_column_1, index_column_2, aggregated_column, cmap="mako"):
     """
@@ -207,8 +207,8 @@ def relationship_viz(
 
 def swarmplot_viz(
     df,
-    xcolumn,
-    ycolumn,
+    colum_1,
+    colum_2,
     groupped_column,
     xlabel,
     ylabel,
@@ -220,8 +220,7 @@ def swarmplot_viz(
     """
     This function gets a Python Pandas dataframe and visualize two countplots.
     :param df: Dataframe to be analyze
-    :param xcolumn: This column shows x axis column.
-    :param ycolumn: This column shows y axis column.
+    :param column_list: list of columns.
     :param groupped_column: Calculation aggregation for this column.
     :param xlabel: It shows name of x axis column.
     :param ylabel: It shows name of y axis column.
@@ -232,9 +231,122 @@ def swarmplot_viz(
     plt.figure(figsize=(15, 10))
     plt.subplot(2, 1, 2)
 
-    sns.swarmplot(x=xcolumn, y=ycolumn, data=df, hue=groupped_column, palette=palette)
+    sns.swarmplot(colum_1,colum_2, data=df, hue=groupped_column, palette=palette)
     plt.xlabel(xlabel, fontsize=fontsize)
     plt.ylabel(ylabel, fontsize=fontsize)
     plt.title(title, fontsize=fontsize_title)
 
     plt.subplots_adjust(hspace=0.5, top=0.9)
+
+    
+
+def boxplot_viz(
+    data,
+    xcolumn,
+    xlabel,
+    title,
+    hue=None,
+    fontsize_label=16,
+    fontsize_title=20,
+    rotation=45,
+    palette="mako",
+):
+    """
+    This function gets a Python Pandas dataframe and visualize a countplot.
+    :param data: Dataframe to be analyze
+    :param xcolumn: This column shows x axis column.
+    :param xlabel: It shows name of x axis column.
+    :param ylabel: It shows name of y axis column.
+    :param title: This column shows name of graph.
+    :return: This function doesn't return anything.
+
+    """
+    plt.figure(1, figsize=(9, 6))
+
+    sns.boxplot(x=xcolumn, data=data, hue=hue, palette=palette)
+    plt.xlabel(xlabel, fontsize=fontsize_label)  # seting the xtitle and size
+    plt.title(title, fontsize=fontsize_title)
+    plt.xticks(rotation=rotation)
+    plt.show()    
+    
+
+    
+def histogram_viz(
+    data,
+    column,
+    separate_column,
+    condition_1,
+    condition_2,
+    label1,
+    label2,
+    color1=None,
+    color2=None,
+):
+    """
+    Gets a Python Pandas dataframe and visualize histogram by a column's conditions.
+    :param data: Dataframe to be analyze
+    :param column: This column is for showing data distribution. 
+    :param separate_column: this colum is for creating histogram by a column's conditions.
+    :param condition_1: It shows condition of separate column.
+    :param condition_2: It shows condition of separate column.
+    :param label1: It shows label by condition_1.
+    :param label2: It shows label by condition_2.
+    :param color1: It shows color for condition_1.
+    :param color2: It shows color for condition_2.
+    :return: This function doesn't return anything.
+
+    """    
+    plt.hist(list(data[data[separate_column] == condition_1][column]), alpha=0.5, label=label1,color=color1)
+    plt.hist(list(data[data[separate_column] == condition_2][column]), alpha=0.5, label=label2,color=color2)
+    plt.legend(loc="upper right")
+    plt.show()   
+    
+    
+
+def distplot_viz(
+    data,
+    column,
+    separate_column,
+    condition_1,
+    condition_2,
+    label1,
+    label2,
+    title,
+    color1=None,
+    color2=None,
+):
+    """
+    Gets a Python Pandas dataframe and visualize displot by a column's conditions. It shows density of column. 
+    :param data: Dataframe to be analyze
+    :param column: This column is for showing data distribution. 
+    :param separate_column: this colum is for creating histogram by a column's conditions.
+    :param condition_1: It shows condition of separate column.
+    :param condition_2: It shows condition of separate column.
+    :param label1: It shows label by condition_1.
+    :param label2: It shows label by condition_2.
+    :param title: It shows title for graph.
+    :param color1: It shows color for condition_1.
+    :param color2: It shows color for condition_2.
+    :return: This function doesn't return anything.
+
+    """    
+    
+    plt.figure(figsize=(15, 5))
+    sns.distplot(
+        data[data[separate_column] == condition_1][column],
+        hist=False,
+        kde=True,
+        kde_kws={"shade": True, "linewidth": 3},
+        label=label1,
+    )
+    sns.distplot(
+        data[data[separate_column] == condition_2][column],
+        hist=False,
+        kde=True,
+        kde_kws={"shade": True, "linewidth": 3},
+        label=label2,
+    )
+    plt.title(title, fontsize=17)
+    plt.xlabel(column, fontsize=15)
+    plt.ylabel("Density", fontsize=15)
+    plt.legend()  
